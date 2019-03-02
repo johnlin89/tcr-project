9d8d6a996f04f6320f3d94b405ac4b750d0f2600
John Lin <linjo@us.ibm.com>
2019-03-02 14:06:03 -0500

    reading in plink linear results

--- snpFilter.R
+++ snpFilter.R
@@ -3,24 +3,32 @@
 # File description comment, including purpose of program, inputs, and outputs
 # source() and library() statements
 
-# if (!requireNamespace("BiocManager"))
-#   install.packages("BiocManager")
-# BiocManager::install()
-# 
-# BiocManager::install('rtracklayer')
-
 #install.packages("KRIS")
-
 #install.packages("pheatmap")
+#install.packages("RCurl")
 library(tidyverse)
 library(rtracklayer)
 library(KRIS)
 library(pheatmap)
 library(LymphoSeq)
+library(RCurl)
 
 # Read in plink files
 plinkRead <- read.bed("/Users/linjo/Desktop/tcr-project-desktop/MIPS_Updated.2019-02-21/data/MIPS_SexCorrected.bed", "/Users/linjo/Desktop/tcr-project-desktop/MIPS_Updated.2019-02-21/data/MIPS_SexCorrected.bim", "/Users/linjo/Desktop/tcr-project-desktop/MIPS_Updated.2019-02-21/data/MIPS_SexCorrected.fam", only.snp = FALSE)
+#http://zzz.bwh.harvard.edu/plink/binary.shtml
+#2 is homozygote A2A2
+#1 is hetero A1A2
+#0 is homozygote?? A1A1
+#NA is missing
 
+
+# Look at EMR Data
+emrAll <- read.delim("/Users/linjo/Box\ Sync/MIPs/Biorepository-Data.V2_2017-06-01_for_BOX.txt",
+                     sep = "\t")
+emr15 <- read.delim("/Users/linjo/Box\ Sync/MIPs/Immunoseq_EHR_2019-02-15_for_BOX.txt",
+                    sep = "\t")
+
+# Create phenotype file 20190301_tcrEmrPheno.txt to input into plink
 # Read in all Phenotypes
 tcrEmrPheno <- read.delim(
   "/Users/linjo/Box\ Sync/MIPs/Immunoseq_EHR_2019-02-15_for_BOX.txt", 
@@ -49,50 +57,15 @@ write_delim(tcrEmrPheno,
 # Define snp window region
 # http://www.imgt.org/IMGTrepertoire/LocusGenes/chromosomes/human/Hu_IGTRloci.html
 tcrbChr <- 7
+# start and end in Mb
 tcrbChrStart <- 141.2
 tcrbChrEnd <- 142
 
+plinkLinear <- read.table("/Users/linjo/Desktop/tcr-project-desktop/MIPS_Updated.2019-02-21/jxl2059/plinkResults/linearTest/MIPS_SexCorrected_Pheno_tcrb_linear.assoc.linear", header = TRUE)
+# Filter for snps with 15 people
+plinkLinear <-  plinkLinear[which(plinkLinear$NMISS == 15),]
+plinkLinear <- plinkLinear[which(plinkLinear$P <= 0.05),]
 
 
 
 
-
-#http://zzz.bwh.harvard.edu/plink/binary.shtml
-
-#2 is homozygote A2A2
-#1 is hetero A1A2
-#0 is homozygote?? A1A1
-#NA is missing
-emr <- read.delim("/Users/linjo/Box\ Sync/MIPs/Biorepository-Data.V2_2017-06-01_for_BOX.txt",
-                  sep = "\t")
-emr2 <- read.delim("/Users/linjo/Box\ Sync/MIPs/Immunoseq_EHR_2019-02-15_for_BOX.txt",
-                   sep = "\t")
-immunoSeq <-  read.delim("/Users/linjo/Box\ Sync/MIPs/ImmunoSeq_exported_samples/201606719-01.tsv",
-                         sep = "\t")
-
-
-
-
-
-
-
-productive.TCR.nt <- productiveSeq(file.list = TCR.list,
-                                   aggregate = "nucleotide", prevalence = FALSE)
-
-TCR.clonality <- clonality(file.list = productive.TCR.nt)
-
-TCR.list <- readImmunoSeq(path = "/Users/linjo/Box Sync/MIPS/ImmunoSeq_exported_samples")
-vGenes <- geneFreq(productive.nt = productive.TCR.nt, locus = "V", family = TRUE)
-library(reshape)
-vGenes <- reshape::cast(vGenes, familyName ~ samples, value = "frequencyGene", sum)
-rownames(vGenes) = as.character(vGenes$familyName)
-vGenes$familyName = NULL
-RedBlue <- grDevices::colorRampPalette(rev(RColorBrewer::brewer.pal(11, "RdBu")))(256)
-pheatmap::pheatmap(vGenes, color = RedBlue, scale = "row")
-
-
-clonalRelatedness(list = TCR.list, editDistance = 10)
-
-
-phyloTree(list = TCR.list, sample = "201606719-01", type = "nucleotide", 
-          layout = "rectangular")
