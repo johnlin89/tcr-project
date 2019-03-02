c3d74a5bc915477a08374f5bdbafab9caa7bfa24
jxl2059 <jxl2059@biolync.EPBI.cwru.edu>
2019-03-02 11:06:31 -0500

    added in phenotypes

--- testingWithPlink2.sh
+++ testingWithPlink2.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-/storage/software/plink --bfile /storage/mips/MIPS_Updated.2019-02-21/data/MIPS_SexCorrected --recode --tab --out MIPS_SexCorrected
+pwd
+
+#output to PED
+/storage/software/plink --bfile /storage/mips/MIPS_Updated.2019-02-21/data/MIPS_SexCorrected --recode --tab --out ../MIPS_SexCorrected
+
+#/storage/software/plink --file /storage/mips/MIPS_Updated.2019-02-21/data/ --pheno 20190301_tcrEmrPheno.txt
+
+/storage/software/plink --bfile /storage/mips/MIPS_Updated.2019-02-21/data/MIPS_SexCorrected --pheno ../20190301_tcrEmrPheno.txt --pheno-name Productive.Clonality -assoc --make-bed --out ../MIPS_SexCorrected_Pheno
 
