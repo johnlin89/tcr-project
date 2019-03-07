df221ffb1792b5e732b4f2f15e304af689731939
John Lin <linjo@us.ibm.com>
2019-03-06 20:43:46 -0500

    first cleaning of first drafts of code

--- lympoSeqAnalysis.R
+++ lympoSeqAnalysis.R
@@ -1,3 +1,7 @@
+# Copyright: CC-BY-SA 4.0
+# Author Notes:  Please note this work is in conjunction with Dr. Dana Crawford and Dr. William Bush at Case Western Reserve Universithy and Cleveland Institute of Computational Biology. The data used below is not my own.
+# File Description: The code below is intended to visualize the immunoSeq data
+
 # if (!requireNamespace("BiocManager"))
 #   install.packages("BiocManager")
 # BiocManager::install()
@@ -6,21 +10,30 @@
 
 library(LymphoSeq)
 
-
-# Read in sample immunoSeq raw file
-immunoSeq <-  read.delim("/Users/linjo/Box\ Sync/MIPs/ImmunoSeq_exported_samples/201606719-01.tsv", sep = "\t")
-
+# TODO: Need to store on the biolync server with RCurl
 # Use LymphoSeq to read in all samples
-TCR.list <- readImmunoSeq(path = "/Users/linjo/Box Sync/MIPS/ImmunoSeq_exported_samples")
+TCR.list <- readImmunoSeq(path = 
+            "/Users/linjo/Box Sync/MIPS/ImmunoSeq_exported_samples")
+
 # Filter for productive sequences
 productive.TCR.nt <- productiveSeq(file.list = TCR.list,
                                    aggregate = "nucleotide", prevalence = FALSE)
 
+# Generate and save visualization plots
+# TODO: use Rcurl to read this in from server
+jpeg('lorenz1.jpg')
+lorenzCurve(samples = names(productive.TCR.nt), list = productive.TCR.nt)
+dev.off()
+jpeg('topSeq1.jpb')
+topSeqsPlot(list = productive.TCR.nt, top = 10)
+dev.off()
+
+### Experimental Code
+
 alignSeq(list = productive.TCR.nt, sample = "201606719-01", type = "nucleotide", 
          method = "ClustalW", output = "consule")
 
-lorenzCurve(samples = names(productive.TCR.nt), list = productive.TCR.nt)
-topSeqsPlot(list = productive.TCR.nt, top = 10)
+
 
 
 TCR.clonality <- clonality(file.list = productive.TCR.nt)
---
+++ tcrManhattan.R
@@ -0,0 +1,83 @@
+# Copyright: CC-BY-SA 4.0
+# Author Notes:  Please note this work is in conjunction with Dr. Dana Crawford and Dr. William Bush at Case Western Reserve Universithy and Cleveland Institute of Computational Biology. The data used below is not my own.
+# File Description: The code below is intended to work with the output from phenoClean.R and the sex-corrected genotype data on the biolync.case.edu server. The goal is to create a Manhattan plot delineating significant SNPs.
+
+require(KRIS)
+require(RCurl)
+require(rtracklayer)
+require(pheatmap)
+require(qqman)
+
+# Read in linear regression results
+# TODO: use Rcurl to read this in from server
+plinkLinear <- read.table("/Users/linjo/Desktop/tcr-project-desktop/MIPS_Updated.2019-02-21/jxl2059/plinkResults/linearTest/MIPS_SexCorrected_Pheno_tcrb_linear.assoc.linear", header = TRUE)
+# Remove where snps with P values of NA
+plinkLinear <- na.omit(plinkLinear, col = "P")
+# Generate and save Manhattan plot
+# TODO: use Rcurl to read this in from server
+jpeg('manhattan1.jpg')
+manhattan(plinkLinear, ylim = c(0, 40), annotatePval = 0.05)
+dev.off()
+# Generate and save qq plot
+# TODO: use Rcurl to read this in from server
+jpeg('qqplot1.jpg')
+qq(plinkLinear$P)
+dev.off()
+
+### Experimental Code
+
+# Read in plink files
+# http://zzz.bwh.harvard.edu/plink/binary.shtml
+# TODO: Need to store on the biolync server with RCurl
+plinkRead <- read.bed("/Users/linjo/Desktop/tcr-project-desktop/MIPS_Updated.2019-02-21/jxl2059/plinkResults/linearTest/MIPS_SexCorrected_Pheno_tcrb_linear.bed", "/Users/linjo/Desktop/tcr-project-desktop/MIPS_Updated.2019-02-21/jxl2059/plinkResults/linearTest/MIPS_SexCorrected_Pheno_tcrb_linear.bim", "/Users/linjo/Desktop/tcr-project-desktop/MIPS_Updated.2019-02-21/jxl2059/plinkResults/linearTest/MIPS_SexCorrected_Pheno_tcrb_linear.fam", 
+                      only.snp = FALSE)
+
+# Creating separate data frames from bed object
+plinkPed <- plinkRead$snp
+plinkPed <- as.data.frame(plinkPed)
+plinkMap <- plinkRead$snp.info
+plinkFam <- plinkRead$ind.info
+
+# Add SNP column names to plinkPed
+# TODO: Verify order of snps from plinkMap can be transposed
+colnames(plinkPed) <- plinkMap$ID
+# Substitute commas for pipes in column names of snps
+colnames(plinkPed)[grep(",", colnames(plinkPed))] <- 
+  gsub(",","|",colnames(plinkPed)[grep(",", colnames(plinkPed))])
+
+# Filter plinkPed for 15 patients with TCR metrics
+# TODO: Verify we can just bind the plinkFam columns - the order is the same?
+plinkPed$FID <- plinkFam$FamID
+plinkPed$IID <- plinkFam$IndID
+# Also, add in phenotypes to plinkPed
+# TODO: Verify - the order is the same?
+plinkPed$phenotype <- plinkFam$phenotype
+# Filter plinkPed for 15 patients with TCR metrics
+plinkPed <- plinkPed[which(plinkPed$IID %in% tcrEmrPheno$IID),]
+
+# Read in productive clonality means associated with each haplotype
+plinkPcMeans <- read.table("/Users/linjo/Desktop/tcr-project-desktop/MIPS_Updated.2019-02-21/jxl2059/plinkResults/linearTest/MIPS_SexCorrected_Pheno_tcrb_linear.qassoc.means", 
+                           header = TRUE)
+# Filter plinkPed for relevant snps
+# Only grab where counts are 5 for each category
+snpFiltered <- plinkPcMeans$SNP[which(plinkPcMeans$VALUE == "COUNTS" & 
+                                        plinkPcMeans$G11 == 5 & 
+                                        plinkPcMeans$G12 == 5 & 
+                                        plinkPcMeans$G11 == 5)]
+plinkPed <- plinkPed[,which(colnames(plinkPed) %in% 
+                              c(as.character(snpFiltered), 
+                                "FID", "IID", "phenotype"))]
+plinkPed <- select(plinkPed, FID, IID, phenotype, everything())
+for (i in 1:length(snpFiltered)) {
+  if (i == 1) {
+    snpFilteredList <- as.character(snpFiltered[i])
+  } else {
+    snpFilteredList <- paste(snpFilteredList, as.character(snpFiltered[i]), sep = ", ")
+  }
+}
+# Manually determine means
+by_snp <- group_by_(plinkPed, .dots = names(plinkPed)[-grep("FID|IID|phenotype", names(plinkPed))])
+by_snp_means <- summarise(by_snp, meanTcr = mean(phenotype, na.rm = TRUE))
+
+
+
