d8d8eaa5478a3a2b4fbc8a2e3dd9ea5e461424bd
jxl2059 <jxl2059@biolync.EPBI.cwru.edu>
2019-03-20 12:45:14 -0400

    new plink code

---
+++ plinkClean.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+pwd
+outputFolder="/storage/mips/MIPS_Updated.2019-02-21/jxl2059/plinkResults/"
+
+# Output PED
+/storage/software/plink --bfile /storage/mips/MIPS_Updated.2019-02-21/data/MIPS_SexCorrected --recode --tab --out $outputFolder/MIPS_SexCorrectedPed
+
+# Add in phenotypes
+/storage/software/plink --bfile /storage/mips/MIPS_Updated.2019-02-21/data/MIPS_SexCorrected --pheno ../tcrEmrPheno.txt --pheno-name Productive.Clonality -assoc --make-bed --out $outputFolder/phenotypeTest/MIPS_SexCorrected_Pheno
+
+# Linear Model with TCRB filtering
+/storage/software/plink --bfile /storage/mips/MIPS_Updated.2019-02-21/data/MIPS_SexCorrected --pheno ../tcrEmrPheno.txt --pheno-name Productive.Clonality --chr 7 --from-mb 141.2 --to-kb 142 --assoc --qt-means --linear --make-bed --out $outputFolder/linearTest/MIPS_SexCorrected_Pheno_tcrb_linear
