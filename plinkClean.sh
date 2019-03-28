3b4fbb4a19b59ea8cd9302355e858575b70df84f
jxl2059 <jxl2059@biolync.EPBI.cwru.edu>
2019-03-27 20:49:12 -0400

    updating newest plink

--- plinkClean.sh
+++ plinkClean.sh
@@ -3,11 +3,11 @@
 pwd
 outputFolder="/storage/mips/MIPS_Updated.2019-02-21/jxl2059/plinkResults/"
 
-# Output PED
-/storage/software/plink --bfile /storage/mips/MIPS_Updated.2019-02-21/data/MIPS_SexCorrected --recode --tab --out $outputFolder/MIPS_SexCorrectedPed
+# updated coordinates for TRB locus, also updated for maf and hwe filtering all in one shot 
+# /storage/software/plink --bfile /storage/mips/MIPS_Updated.2019-02-21/data/MIPS_SexCorrected --pheno ../tcrEmrPheno.txt --pheno-name Productive.Clonality --chr 7 --from-bp 141998851 --to-bp 142510972 --maf 0.1 --hwe 0.0001 --assoc --qt-means --linear --make-bed --out $outputFolder/plinkFiltering/plink1
 
-# Add in phenotypes
-/storage/software/plink --bfile /storage/mips/MIPS_Updated.2019-02-21/data/MIPS_SexCorrected --pheno ../tcrEmrPheno.txt --pheno-name Productive.Clonality -assoc --make-bed --out $outputFolder/phenotypeTest/MIPS_SexCorrected_Pheno
+# output after MAF and HWE Filtering
+/storage/software/plink --bfile /storage/mips/MIPS_Updated.2019-02-21/data/MIPS_SexCorrected --pheno ../tcrEmrPheno.txt --pheno-name Productive.Clonality --chr 7 --from-bp 141998851 --to-bp 142510972 --maf 0.1 --hwe 0.0001 --make-bed --out $outputFolder/plinkFiltering/plink2 --freq --counts
 
-# Linear Model with TCRB filtering
-/storage/software/plink --bfile /storage/mips/MIPS_Updated.2019-02-21/data/MIPS_SexCorrected --pheno ../tcrEmrPheno.txt --pheno-name Productive.Clonality --chr 7 --from-mb 141.2 --to-kb 142 --assoc --qt-means --linear --make-bed --out $outputFolder/linearTest/MIPS_SexCorrected_Pheno_tcrb_linear
+# prune
+/storage/software/plink --bfile /storage/mips/MIPS_Updated.2019-02-21/data/MIPS_SexCorrected --pheno ../tcrEmrPheno.txt --pheno-name Productive.Clonality --chr 7 --from-bp 141998851 --to-bp 142510972 --maf 0.1 --hwe 0.0001 --make-bed --out $outputFolder/plinkFiltering/plink3 --freq --counts --prune
