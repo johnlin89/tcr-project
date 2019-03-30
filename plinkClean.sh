e886a468c707a99b119cfddd156bd372c213752e
John Lin <jxl2059@case.edu>
2019-03-30 18:06:21 -0400

    getting rid of assoc option. not sure why I initially did this

--- plinkClean.sh
+++ plinkClean.sh
@@ -6,7 +6,7 @@ outputFolder="/storage/mips/MIPS_Updated.2019-02-21/jxl2059/plinkResults/"
 # updated coordinates for TRB locus, also updated for maf and hwe filtering all in one shot 
 rm -r $outputFolder/plinkFiltering/plink1
 mkdir $outputFolder/plinkFiltering/plink1
-/storage/software/plink --bfile /storage/mips/MIPS_Updated.2019-02-21/data/MIPS_SexCorrected --pheno ../tcrEmrPheno.txt --pheno-name Productive.Clonality --chr 7 --from-bp 141998851 --to-bp 142510972 --maf 0.1 --hwe 0.0001 --assoc qt-means --linear --make-bed --out $outputFolder/plinkFiltering/plink1/updatedCoord
+/storage/software/plink --bfile /storage/mips/MIPS_Updated.2019-02-21/data/MIPS_SexCorrected --pheno ../tcrEmrPheno.txt --pheno-name Productive.Clonality --chr 7 --from-bp 141998851 --to-bp 142510972 --maf 0.1 --hwe 0.0001 --linear --make-bed --out $outputFolder/plinkFiltering/plink1/updatedCoord
 # 600 SNPs in region (1824820 snps total)
 # 489 removed due to MAF, 3 due to HWE
 
@@ -29,17 +29,17 @@ mkdir $outputFolder/plinkFiltering/plink3
 # do linear regressions after pruning for 15
 rm -r $outputFolder/plinkFiltering/plink4
 mkdir $outputFolder/plinkFiltering/plink4
-/storage/software/plink --bfile /storage/mips/MIPS_Updated.2019-02-21/jxl2059/plinkResults/plinkFiltering/plink3/prune --pheno ../tcrEmrPheno.txt --pheno-name Productive.Clonality --chr 7 --from-bp 141998851 --to-bp 142510972 --assoc --linear --make-bed --out $outputFolder/plinkFiltering/plink4/pruneLinear --freq counts
+/storage/software/plink --bfile /storage/mips/MIPS_Updated.2019-02-21/jxl2059/plinkResults/plinkFiltering/plink3/prune --pheno ../tcrEmrPheno.txt --pheno-name Productive.Clonality --chr 7 --from-bp 141998851 --to-bp 142510972 --linear --make-bed --out $outputFolder/plinkFiltering/plink4/pruneLinear --freq counts
 
 # expand window by 50000 bp on each side and do linear regressions
 rm -r $outputFolder/plinkFiltering/plink5
 mkdir $outputFolder/plinkFiltering/plink5
-/storage/software/plink --bfile /storage/mips/MIPS_Updated.2019-02-21/jxl2059/plinkResults/plinkFiltering/plink3/prune --pheno ../tcrEmrPheno.txt --pheno-name Productive.Clonality --chr 7 --from-bp 141948851 --to-bp 142560972 --assoc --linear --make-bed --out $outputFolder/plinkFiltering/plink5/window --freq counts
+/storage/software/plink --bfile /storage/mips/MIPS_Updated.2019-02-21/jxl2059/plinkResults/plinkFiltering/plink3/prune --pheno ../tcrEmrPheno.txt --pheno-name Productive.Clonality --chr 7 --from-bp 141948851 --to-bp 142560972 --linear --make-bed --out $outputFolder/plinkFiltering/plink5/window --freq counts
 
 # see all of chr 7 with pruned individuals, and MAF and HWE Filtering
 rm -r $outputFolder/plinkFiltering/plink6
 mkdir $outputFolder/plinkFiltering/plink6
-/storage/software/plink --bfile /storage/mips/MIPS_Updated.2019-02-21/data/MIPS_SexCorrected --pheno ../tcrEmrPheno.txt --pheno-name Productive.Clonality --chr 7 --maf 0.1 --hwe 0.0001 --assoc --linear --make-bed --out $outputFolder/plinkFiltering/plink6/allChr7 --freq counts --prune
+/storage/software/plink --bfile /storage/mips/MIPS_Updated.2019-02-21/data/MIPS_SexCorrected --pheno ../tcrEmrPheno.txt --pheno-name Productive.Clonality --chr 7 --maf 0.1 --hwe 0.0001 --linear --make-bed --out $outputFolder/plinkFiltering/plink6/allChr7 --freq counts --prune
 
 # add in covariates, building off  of code for plink5
 rm -r $outputFolder/plinkFiltering/plink7
