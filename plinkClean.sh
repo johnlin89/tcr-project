b735349f353690f7bd7d77f3ff4025938967af8a
John Lin <jxl2059@case.edu>
2019-04-23 21:09:04 -0400

    adding plink command for processing ALL 129 samples for admixture

--- plinkClean.sh
+++ plinkClean.sh
@@ -37,15 +37,32 @@ rm -r $outputFolder/plinkFiltering/plink5
 mkdir $outputFolder/plinkFiltering/plink5
 /storage/software/plink --bfile /storage/mips/MIPS_Updated.2019-02-21/data/MIPS_SexCorrected --pheno ../phenotypes/tcrEmrPheno.txt --pheno-name Productive.Clonality --chr 7 --from-bp 141948851 --to-bp 142560972  --maf 0.1 --hwe 0.0001 --linear --make-bed --out $outputFolder/plinkFiltering/plink5/window --freq counts --prune
 # 654 snps in window
-# 495 removed due to MAF, 0 removed due to HWE
+# 495 removed due to MAF, 1 removed due to HWE
 # 159 SNPs left
 
-# see all of chr 7 with pruned individuals, and MAF and HWE Filtering
-# rm -r $outputFolder/plinkFiltering/plink6
-# mkdir $outputFolder/plinkFiltering/plink6
-# /storage/software/plink --bfile /storage/mips/MIPS_Updated.2019-02-21/data/MIPS_SexCorrected --pheno ../phenotypes/tcrEmrPheno.txt --pheno-name Productive.Clonality --chr 7 --maf 0.1 --hwe 0.0001 --linear --make-bed --out $outputFolder/plinkFiltering/plink6/allChr7 --freq counts --prune
 
-# fix sex coding
-# rm -r $outputFolder/plinkFiltering/sexCoding
-# mkdir $outputFolder/plinkFiltering/sexCoding
-# /storage/software/plink --bfile /storage/mips/MIPS_Updated.2019-02-21/jxl2059/plinkResults/plinkFiltering/plink3/prune --update-sex ../phenotypes/tcrEmrPheno.txt
+# see all of chr 7 with pruned individuals, and MAF and HWE Filtering
+rm -r $outputFolder/plinkFiltering/plink6
+mkdir $outputFolder/plinkFiltering/plink6
+/storage/software/plink --bfile /storage/mips/MIPS_Updated.2019-02-21/data/MIPS_SexCorrected --pheno ../phenotypes/tcrEmrPheno.txt --pheno-name Productive.Clonality --chr 7 --maf 0.1 --hwe 0.0001 --linear --make-bed --out $outputFolder/plinkFiltering/plink6/allChr7 --freq counts --prune
+# 97332 snps in window
+# 63571 removed due to MAF, 1 removed due to HWE
+# 33760 SNPs left
+
+# use age and sex as covariates and redo linear regressions, building off of expanded window from plink5
+rm -r $outputFolder/plinkFiltering/plink7
+mkdir $outputFolder/plinkFiltering/plink7
+/storage/software/plink --bfile $outputFolder/plinkFiltering/plink5/window --covar ../phenotypes/tcrEmrPheno.txt  --covar-name SEX,AGE --linear --make-bed --out $outputFolder/plinkFiltering/plink7/windowCovar
+# 159 SNPs
+
+# use all samples (not just 15) to estimate admixture
+# use expanded window, 50,000 bp on each side of TRB locus
+# got rid of pheno options and linear modeling
+rm -r $outputFolder/plinkFiltering/plink8
+mkdir $outputFolder/plinkFiltering/plink8
+/storage/software/plink --bfile /storage/mips/MIPS_Updated.2019-02-21/data/MIPS_SexCorrected --chr 7 --from-bp 141948851 --to-bp 142560972  --maf 0.1 --hwe 0.0001 --make-bed --out $outputFolder/plinkFiltering/plink8/allWindow --freq counts
+# 129 people (61 males, 68 females)
+# 654 snps from file
+# 3 snps removed due to hwe
+# 527 removed due to MAF
+# 124 variants removed
