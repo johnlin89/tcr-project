8a55288030064c81c2e6648b1b229654a99bd794
John Lin <linjo@us.ibm.com>
2019-04-24 20:38:06 -0400

    plotting admixture results

--- admixture1.R
+++ admixture1.R
@@ -1,3 +1,20 @@
+
+library(ssh)
+library(tidyverse)
+library(dplyr)
+# merge with phenotypes
+# Connect to biolync server
+session  <- ssh_connect("jxl2059@biolync.case.edu")
+
+scp_download(session, "/storage/mips//MIPS_Updated.2019-02-21/jxl2059/tcr-project/allWindowNoDup_flipRefPruned.2.Q", to = "/Users/linjo/GoogleDrive/CaseWesternUniversity/tcr-project/data")
+test <- read.table(
+  "/Users/linjo/GoogleDrive/CaseWesternUniversity/tcr-project/data/allWindowNoDup_flipRefPruned.2.Q")
+colnames(test) <- c("CEU", "YRI")
+barplot(t(as.matrix(test)), col = rainbow(3),
+        xlab = "Individual #", ylab = "Ancestry", border = NA, legend.text = TRUE)
+#green is YRI
+#red is CEU
+
 tbl1 = read.table("/Users/linjo/Desktop/tcr-project-desktop/snpFlipResults/windowCovar_flip.1.Q")
 jpeg('figures/admixture1K1.jpg')
 barplot(t(as.matrix(tbl1)), col = rainbow(3),
