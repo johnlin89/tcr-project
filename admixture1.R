47f610ed932833fe4e2ed54e90554f93cc3bf351
John Lin <linjo@us.ibm.com>
2019-05-01 15:13:49 -0400

    cleaning admixture R code

--- admixture1.R
+++ admixture1.R
@@ -2,14 +2,28 @@
 library(ssh)
 library(tidyverse)
 library(dplyr)
-# merge with phenotypes
+
 # Connect to biolync server
 session  <- ssh_connect("jxl2059@biolync.case.edu")
 
-scp_download(session, "/storage/mips//MIPS_Updated.2019-02-21/jxl2059/admixtureResults/allWindowNoDup_flipRefPruned.2.P", to = "/Users/linjo/GoogleDrive/CaseWesternUniversity/tcr-project/data")
-scp_download(session, "/storage/mips//MIPS_Updated.2019-02-21/jxl2059/admixtureResults/allWindowNoDup_flipRefPruned.2.Q_se", to = "/Users/linjo/GoogleDrive/CaseWesternUniversity/tcr-project/data")
-scp_download(session, "/storage/mips//MIPS_Updated.2019-02-21/jxl2059/admixtureResults/allWindowNoDup_flipRefPruned.2.Q_bias", to = "/Users/linjo/GoogleDrive/CaseWesternUniversity/tcr-project/data")
-
+# merge with phenotypes
+scp_download(session, "/storage/mips/MIPS_Updated.2019-02-21/jxl2059/plinkResults/plinkFiltering/plink15/allWindowNoDup_flipRefPruned.fam", to = "/Users/linjo/GoogleDrive/CaseWesternUniversity/tcr-project/data")
+scp_download(session, "/storage/mips//MIPS_Updated.2019-02-21/data/SampleInfo/MIPs_genotyped_2019-02-15.txt", to = "/Users/linjo/GoogleDrive/CaseWesternUniversity/tcr-project/data")
+fam <- read.table(
+  "/Users/linjo/GoogleDrive/CaseWesternUniversity/tcr-project/data/allWindowNoDup_flipRefPruned.fam")
+colnames(fam) <- c("FID", "IID", "PaternalID", "MaternalID", "SEX", "TCR")
+eth <- read.delim(
+  "/Users/linjo/GoogleDrive/CaseWesternUniversity/tcr-project/data/MIPs_genotyped_2019-02-15.txt", 
+  sep = "\t")
+eth$IID <- paste(substring(eth$Study.ID, 1, 4), substring(eth$Study.ID, 10, 12),
+                 sep = "")
+eth <- select(eth, IID, Race)
+eth$Race[which(eth$Race == "")] = "unknown"
+eth$Race <- as.factor(eth$Race)
+eth <- unique(eth)
+ethFam <- merge(eth, fam, by = "IID", all.y = TRUE)
+ethFam <- select(ethFam, -FID, -PaternalID,  -MaternalID)
+save(ethFam, file = "/Users/linjo/GoogleDrive/CaseWesternUniversity/tcr-project/data/ethFam")
 
 # Method 1: Unsupervised learning
 scp_download(session, "/storage/mips//MIPS_Updated.2019-02-21/jxl2059/admixtureResults/admixture3/allWindow.2.Q", to = "/Users/linjo/GoogleDrive/CaseWesternUniversity/tcr-project/data")
@@ -20,7 +34,6 @@ cv <- read.table(
   "/Users/linjo/GoogleDrive/CaseWesternUniversity/tcr-project/data/crossValidation.txt")
 jpeg('figures/admixture1CV.jpg')
 plot(cv$V4, xlab = "K", ylab = "CV Error", main = "Admixture Method 1: 10-fold Cross Validation Results", type = "l")
-line(data = cv$V4)
 dev.off()
 jpeg('figures/admixture1.jpg')
 barplot(t(as.matrix(admixture1)), col = rainbow(3),
@@ -33,44 +46,29 @@ scp_download(session, "/storage/mips//MIPS_Updated.2019-02-21/jxl2059/admixtureR
 admixture2 <- read.table(
   "/Users/linjo/GoogleDrive/CaseWesternUniversity/tcr-project/data/allWindowNoDup_flipRefPruned.2.Q")
 colnames(admixture2) <- c("CEU", "YRI")
+# Bind with self reported races and other columns
+admixture2 <- cbind(ethFam, admixture2)
+# Order by percentage of race
+admixture2 <- admixture2[order(admixture2$CEU), ]
+# Get rid of reference populations
+admixture2 <- admixture2[-grep("NA", admixture2$IID), ]
+# Create column to see if results agree with self reported race
+admixture2$raceTest = NA
+admixture2$raceTest[which(admixture2$CEU < admixture2$YRI & 
+                            admixture2$Race == "African American")] = 1
+admixture2$raceTest[which(admixture2$CEU < admixture2$YRI & 
+                            admixture2$Race == "European American")] = 0
 jpeg('figures/admixture2.jpg')
-barplot(t(as.matrix(admixture2)), col = rainbow(3),
-        xlab = "Individual #", ylab = "Ancestry", border = NA, 
+barplot(t(as.matrix(select(admixture2, CEU, YRI))), col = rainbow(3), 
+        xlab = "Individuals", ylab = "Ancestry", border = NA, axisnames = FALSE, 
         legend.text = TRUE, main = "Admixture Method 2: Ancestry Proportions")
 #green is YRI
 #red is CEU
 dev.off()
-
-tbl1 = read.table("/Users/linjo/Desktop/tcr-project-desktop/snpFlipResults/windowCovar_flip.1.Q")
-jpeg('figures/admixture1K1.jpg')
-barplot(t(as.matrix(tbl1)), col = rainbow(3),
-        xlab = "Individual #", ylab = "Ancestry", border = NA)
-dev.off()
-tbl2 = read.table("/Users/linjo/Desktop/tcr-project-desktop/snpFlipResults/windowCovar_flip.2.Q")
-jpeg('figures/admixture1K2.jpg')
-barplot(t(as.matrix(tbl2)), col = rainbow(3),
-        xlab = "Individual #", ylab = "Ancestry", border = NA)
-dev.off()
-tbl3 = read.table("/Users/linjo/Desktop/tcr-project-desktop/snpFlipResults/windowCovar_flip.3.Q")
-jpeg('figures/admixture1K3.jpg')
-barplot(t(as.matrix(tbl3)), col = rainbow(3),
-        xlab = "Individual #", ylab = "Ancestry", border = NA)
-dev.off()
-tbl4 = read.table("/Users/linjo/Desktop/tcr-project-desktop/snpFlipResults/windowCovar_flip.4.Q")
-jpeg('figures/admixture1K4.jpg')
-barplot(t(as.matrix(tbl4)), col = rainbow(3),
-        xlab = "Individual #", ylab = "Ancestry", border = NA)
-dev.off()
-tbl5 = read.table("/Users/linjo/Desktop/tcr-project-desktop/snpFlipResults/windowCovar_flip.5.Q")
-jpeg('figures/admixture1K5.jpg')
-barplot(t(as.matrix(tbl5)), col = rainbow(3),
-        xlab = "Individual #", ylab = "Ancestry", border = NA)
-dev.off()
+save(admixture2, file = "/Users/linjo/GoogleDrive/CaseWesternUniversity/tcr-project/data/admixture2")
 
 
-### all samples
-tbl6 = read.table("/Users/linjo/Desktop/tcr-project-desktop/snpFlipResults/allWindow_flip.5.Q")
-jpeg('figures/allAdmixture1K1.jpg')
-barplot(t(as.matrix(tbl6)), col = rainbow(3),
-        xlab = "Individual #", ylab = "Ancestry", border = NA)
-dev.off()
+# Save plots to biolync server
+scp_upload(session, 'figures/admixture1.jpg', to = "/storage/mips/MIPS_Updated.2019-02-21/jxl2059/figures")
+scp_upload(session, 'figures/admixture2.jpg', to = "/storage/mips/MIPS_Updated.2019-02-21/jxl2059/figures")
+
